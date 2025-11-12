5d0a4388144dd63e7d61c11ee3b6ace86ac1b9f6
Lo <lovisaornestal@gmail.com>
2025-11-12 12:58:43 +0100

    fetch

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
 import './style.css'
 
+const getGiphy = async() => {
+  const response = await fetch("https://api.giphy.com/v1/gifs/random?api_key=P97zFDhcpVpYhn8G9Z5UNOR69ORP0dJA");
+  const data = await response.json();
+  console.log(data)
+  
+}
+
+getGiphy();
