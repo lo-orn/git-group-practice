0c338c83d0611b79a2c442986813cc95e74cbfea
Sintia Saado <sintia@MacBook-Air-som-tillhor-Sintia.local>
2025-11-12 13:47:16 +0100

    test

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,11 @@
-import "./style.css";
+import './style.css'
 
-// MODELS
-type GiphyResponse = {
-  data: Gif[];
-};
-
-type Gif = {
-  id: string;
-  images: ImageSize[];
-};
-
-type ImageSize = {
-  original: OriginalSize[];
-};
-
-type OriginalSize = {
-  url: string;
-};
-//END MODELS
-const getGiphy = async () => {
+const getGiphy = async() => {
   const response = await fetch("https://api.giphy.com/v1/gifs/random?api_key=P97zFDhcpVpYhn8G9Z5UNOR69ORP0dJA");
   const data = await response.json();
-  console.log(data);
-};
+  console.log(data)
+  
+}
 
 function handleClick(): void {
   alert("Knappen klickades!");
