e1b9921af1df88e9cb87d559cf8833a767636fdf
Frida Palm <fridapalm@MacBook-Pro-som-tillhor-Frida.local>
2025-11-12 13:13:16 +0100

    added models

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
-import './style.css'
+import "./style.css";
 
+type GiphyResponse = {
+  data: Gif[];
+};
+
+type Gif = {
+  id: string;
+  images: ImageSize[];
+};
+
+type ImageSize = {
+  original: OriginalSize[];
+};
+
+type OriginalSize = {
+  url: string;
+};
